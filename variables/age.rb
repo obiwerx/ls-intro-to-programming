8467b9c186f80fefee49c94c80dc18c1047dff67
Tim O'Brien <timothy.p.obrien@gmail.com>
2017-06-24 13:42:08 -0400

    Variables Chapter Exercises are complete.

---
+++ variables/age.rb
@@ -0,0 +1,9 @@
+# /usr/bin/env ruby
+
+puts "How old are you?"
+age = gets
+
+puts "in 10 years you will be " + (age.to_i + 10).to_s
+puts "in 20 years you will be " + (age.to_i + 20).to_s
+puts "in 30 years you will be " + (age.to_i + 30).to_s
+puts "in 40 years you will be " + (age.to_i + 40).to_s
---
+++ variables/name.rb
@@ -0,0 +1,10 @@
+# /usr/bin/env ruby
+puts "What is your first name?:"
+first_name = gets.chomp
+puts "What is your last name?:"
+last_name = gets.chomp
+puts "Hello, #{first_name} #{last_name}"
+
+10.times do
+  puts "#{first_name} #{last_name}"
+end
